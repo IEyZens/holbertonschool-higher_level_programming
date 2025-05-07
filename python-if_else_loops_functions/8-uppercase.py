58717d802fb340843a46ec481cd44210e960021d
IEyZens <roncint61@gmail.com>
2025-05-07 11:32:20 +0200

    Add file for task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    result = ""
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            result += chr(ord(i) - 32)
+        else:
+            result += i
+    print("{}".format(result))
