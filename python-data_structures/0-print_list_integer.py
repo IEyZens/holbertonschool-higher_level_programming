2aefab87da2bd45a7862a58f2b73273401ea527f
IEyZens <roncint61@gmail.com>
2025-05-12 12:12:51 +0200

    Update for pycodestyle condition

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]): # Function to print all integers of a list
-    for i in my_list: # Loop through the list
-        print("{}".format(i)) # Print each integer
+# Function to print all integers of a list
+def print_list_integer(my_list=[]):
+    # Loop through the list
+    for i in my_list:
+        # Print each integer
+        print("{}".format(i))
