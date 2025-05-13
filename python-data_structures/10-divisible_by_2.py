eac1290fdd0953c0c9574854bf20ee3d8b9d9fd7
IEyZens <roncint61@gmail.com>
2025-05-13 12:00:04 +0200

    Add divisible_by_2 function to check divisibility by 2 in a list

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    # Create a new list to store the results
+    new_list=[]
+    # Loop through the list
+    for i in my_list:
+        # Check if the integer is divisible by 2
+        if i % 2 == 0:
+            # Append True to the new list if divisible by 2
+            new_list.append(True)
+        # Append False to the new list if not divisible by 2
+        else:
+            # Append False to the new list
+            new_list.append(False)
+    # Return the new list
+    return new_list
