fb3043257d2a3fbe18ee144f0493d21fc0c33e1b
IEyZens <roncint61@gmail.com>
2025-05-14 10:55:21 +0200

    Clarify comment in update_dictionary function to explain key addition behavior

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,4 +3,5 @@
 def update_dictionary(a_dictionary, key, value):
     # Check if the key exists in the dictionary
     a_dictionary[key] = value
+    # If the key does not exist, it will be added with the given value
     return a_dictionary
