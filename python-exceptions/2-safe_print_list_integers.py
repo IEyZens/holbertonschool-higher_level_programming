071a2c96ce06c6316644f8dbc525042f413d4a93
IEyZens <roncint61@gmail.com>
2025-05-14 15:20:58 +0200

    Fix print formatting in safe_print_list_integers to include space between printed integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
         # Try to print the element at index i
         try:
             # Check if the index is within the range of the list
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end=" ")
         # If the index is out of range, catch the IndexError
         except IndexError:
             # Print a new line and break the loop
