aa41c54c9332d8b7e1c5447e2cb506b006459d1b
IEyZens <roncint61@gmail.com>
2025-05-14 15:00:35 +0200

    Add safe_print_integer function to print integers safely

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    # Prints an integer with "{:d}".format()
+    try:
+        # Try to print the value as an integer
+        print("{:d}".format(value))
+    # If the value is not an integer, catch the TypeError
+    except:
+        # Print a new line and return False
+        return False
+    # If the value is an integer, return True
+    else:
+        # Return True if the value is an integer
+        return True
