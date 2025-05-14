c2d1a7fd7bbc8ae468fefdb53e9c6b5224b23539
IEyZens <roncint61@gmail.com>
2025-05-14 11:31:01 +0200

    Add best_score function to find the key with the maximum value in a dictionary

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    # Check if the dictionary is empty
+    if a_dictionary == {}:
+        # Return None if the dictionary is empty
+        return None
+    # Find the key with the maximum value in the dictionary
+    return max(a_dictionary, key=a_dictionary.get)
