634839b49a4b88bf715bc6de877a8e1ed6efa08a
IEyZens <roncint61@gmail.com>
2025-05-14 10:10:19 +0200

    Refactor variable names in square_matrix_simple for improved readability

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,11 +4,11 @@ def square_matrix_simple(matrix=[]):
     # Create a new matrix with the same dimensions as the input matrix
     new_matrix = []
     # Iterate through each row of the input matrix
-    for i in matrix:
+    for row in matrix:
         # Create a new row for the new matrix
         square_line = []
         # Iterate through each element in the row
-        for j in i:
+        for j in row:
             # Append the square of the element to the new row
             square_line.append(j ** 2)
         # Append the new row to the new matrix
