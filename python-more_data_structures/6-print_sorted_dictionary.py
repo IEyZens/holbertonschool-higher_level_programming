ff2dcd3c5eed23bc49e8b3663156da10d070433d
IEyZens <roncint61@gmail.com>
2025-05-14 10:45:45 +0200

    Add print_sorted_dictionary function to print a dictionary sorted by keys

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    # Sort the dictionary by keys and print each key-value pair
+    for key in sorted(a_dictionary):
+        # Print the key and its corresponding value
+        print(key, ":", a_dictionary[key])
