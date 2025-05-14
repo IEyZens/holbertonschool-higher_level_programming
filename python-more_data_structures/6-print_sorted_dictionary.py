a9aeb3469b72e5bb8648a94f8f996a158dfe18c4
IEyZens <roncint61@gmail.com>
2025-05-14 10:47:37 +0200

    Fix print format in print_sorted_dictionary function to use f-string for better readability

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(a_dictionary):
     # Sort the dictionary by keys and print each key-value pair
     for key in sorted(a_dictionary):
         # Print the key and its corresponding value
-        print(key, ":", a_dictionary[key])
+        print(f"{key}: {a_dictionary[key]}")
