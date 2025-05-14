fe2971c48316eb120bd8bd60c5c7f2c053e78143
IEyZens <roncint61@gmail.com>
2025-05-14 11:44:16 +0200

    Add docstring to multiply_list_map function for clarity

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
+    # Create a new list by multiplying each element in my_list by number
     return list(map(lambda x: x * number, my_list))
