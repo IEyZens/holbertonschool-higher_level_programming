1691d170bb159e92bc49f065f932ce23c0fabd13
IEyZens <roncint61@gmail.com>
2025-05-14 15:02:05 +0200

    Fix exception handling in safe_print_integer to catch specific errors

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -6,7 +6,7 @@ def safe_print_integer(value):
         # Try to print the value as an integer
         print("{:d}".format(value))
     # If the value is not an integer, catch the TypeError
-    except:
+    except (ValueError, TypeError):
         # Print a new line and return False
         return False
     # If the value is an integer, return True
