d4aadc2e4dac81e71d94f90c53531dba94170436
IEyZens <roncint61@gmail.com>
2025-05-15 14:36:29 +0200

    Enhance input validation comments in add_integer function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
+    # Validate the input types
     if not isinstance(a, (int, float)):
+        # Check if a is an integer or float
         raise TypeError("a must be an integer")
+    # Check if b is an integer or float
     if not isinstance(b, (int, float)):
+        # Check if b is an integer or float
         raise TypeError("b must be an integer")
 
+    # Convert float to int
     return int(a) + int(b)
