7f2ab4053f28b349d1a3dbf4dc919408bfb0b85a
IEyZens <roncint61@gmail.com>
2025-05-16 10:42:33 +0200

    Remove unnecessary strip() from print statement in say_my_name function

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -21,4 +21,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     # Print the full name
-    print("My name is {} {}".format(first_name, last_name).strip())
+    print("My name is {} {}".format(first_name, last_name))
