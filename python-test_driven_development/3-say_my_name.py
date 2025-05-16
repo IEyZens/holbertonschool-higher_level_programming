7f86f404d982d1382c297d97b397e09df6608091
IEyZens <roncint61@gmail.com>
2025-05-16 10:37:57 +0200

    Refactor type checks in say_my_name function for consistency and clarity

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -12,13 +12,13 @@ def say_my_name(first_name, last_name=""):
         TypeError: If either of first_name or last_name are not strings.
     """
     # Check if first_name is a string
-    if not isinstance(first_name, (str)):
+    if not isinstance(first_name, str):
         # Check if first_name is a string
         raise TypeError("first_name must be a string")
     # Check if last_name is a string
-    if not isinstance(last_name, (str)):
+    if not isinstance(last_name, str):
         # Check if last_name is a string
         raise TypeError("last_name must be a string")
 
     # Print the full name
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name).strip())
