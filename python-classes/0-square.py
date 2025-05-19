a3da8b5cec77ca10bb767121ce3cebe91e0c2335
IEyZens <roncint61@gmail.com>
2025-05-19 11:15:38 +0200

    Add empty Square class as a placeholder for future development

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square.
+It serves as a placeholder for future development.
+The class does not contain any attributes or methods.
+"""
+
+
+class Square:
+    """This is an empty class that defines a square."""
+    pass
