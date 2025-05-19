f15aa83678c7e862cd510871b52fd191ef86251c
IEyZens <roncint61@gmail.com>
2025-05-19 11:30:01 +0200

    Add Square class with size validation in constructor

---
+++ python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square.
+It serves as a placeholder for future development.
+The class does not contain any attributes or methods.
+"""
+
+
+class Square:
+    """This is an empty class that defines a square."""
+    __size = None
+
+    """
+    This is a constructor method that initializes the size of the square.
+    :param size: The size of the square.
+    """
+    def __init__(self, size):
+        # Check if size is an integer
+        if not isinstance(size, int):
+            # raise an exception if size is not an integer
+            raise TypeError("size must be an integer")
+        # Check if size is less than 0
+        if size < 0:
+            # raise an exception if size is less than 0
+            raise ValueError("size must be >= 0")
+
+        # Set the size of the square
+        self.__size = size
