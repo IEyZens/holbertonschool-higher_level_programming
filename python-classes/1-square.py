407966e14ddd1bf30637bfe187aee9b1653fa070
IEyZens <roncint61@gmail.com>
2025-05-19 11:23:51 +0200

    Add Square class with constructor to initialize size

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square.
+It serves as a placeholder for future development.
+The class does not contain any attributes or methods.
+"""
+
+
+class Square:
+    """"
+    This is an empty class that defines a square.
+    """
+    __size = None
+
+    """
+    This is a constructor method that initializes the size of the square.
+    :param size: The size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size
