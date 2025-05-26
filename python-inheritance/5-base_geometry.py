1edddf60fcd2da91ddf6ac02d32c9db132ea7268
IEyZens <roncint61@gmail.com>
2025-05-26 21:47:04 +0200

    Add initial implementation of BaseGeometry class with docstring

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a function that returns a list of available attributes and methods
+    of an object.
+    the object.
+    """
+
+
+class BaseGeometry:
+    """An empty class BaseGeometry."""
+    pass
