eca7c6cdca04c8934df4193d304ec956a4e786ab
IEyZens <roncint61@gmail.com>
2025-05-26 22:12:22 +0200

    Remove unused main block and clean up whitespace in BaseGeometry class

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -36,8 +36,3 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-
-
-if __name__ == "__main__":
-    bg = BaseGeometry()
-    bg.integer_validator("size", 10)
