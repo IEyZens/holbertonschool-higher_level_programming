50ae2d5ec489563007a5330c593023b37c42d154
IEyZens <roncint61@gmail.com>
2025-05-26 21:45:05 +0200

    Fix docstring formatting in inherits_from function for clarity

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,14 +5,12 @@
 
 
 def inherits_from(obj, a_class):
-    """Check if an object is an instance of a class that inherited (directly
-    or indirectly)
+    """Check if an object is an instance of a class that inherited (directly or indirectly)
     Args:
         obj: The object to inspect.
         a_class: The class to check against.
     Returns:
-        bool: True if obj is an instance of a_class that inherited (directly
-        or indirectly)
+        bool: True if obj is an instance of a_class that inherited (directly or indirectly)
               from a_class, otherwise False.
     """
     if isinstance(obj, a_class) and type(obj) is not a_class:
