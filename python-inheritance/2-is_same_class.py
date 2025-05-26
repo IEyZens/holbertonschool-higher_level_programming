8ff2cd47986ac114fcd6ff16a3132d5547aaaf33
IEyZens <roncint61@gmail.com>
2025-05-26 21:27:31 +0200

    Refactor docstring in is_same_class function for clarity and completeness

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""Defines a class MyList that inherits from list and adds a method to print
+the list in sorted order.
+"""
 
 
 def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of a specified class.
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+    Returns:
+        bool: True if obj is an instance of a_class, False otherwise.
+    """
     # Check if obj is exactly an instance of a_class.
     return type(obj) == a_class
