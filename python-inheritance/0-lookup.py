36f74cfc2a158d4c5ade16c33fad8afc6938cd00
IEyZens <roncint61@gmail.com>
2025-05-26 21:08:33 +0200

    Fix docstring for lookup function to clarify its purpose

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Defines a function that returns a list of available attributes and method"""
+
 
 def lookup(obj):
     """Return a list of available attributes and methods of an object.
