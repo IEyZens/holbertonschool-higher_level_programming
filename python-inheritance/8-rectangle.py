1a9b7c2ff72f70e4e61c55d48ae18c0e4b997619
IEyZens <roncint61@gmail.com>
2025-05-28 04:24:27 +0200

    Add a blank line after the module docstring for improved readability

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,6 +5,7 @@ and integer validation.
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """
     Represents a rectangle, inheriting from BaseGeometry.
