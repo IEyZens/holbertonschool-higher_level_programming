01343798cd7b25b44c7ecb8f02d5e85238b4e284
IEyZens <roncint61@gmail.com>
2025-05-28 04:14:13 +0200

    Refactor docstrings in BaseGeometry class for clarity and consistency

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
 """
-Defines a base class for geometric shapes with methods for area calculation and integer validation.
+Defines a base class for geometric shapes with methods for area calculation
+and integer validation.
 """
-
 
 class BaseGeometry:
     """
-    Classe de base pour des formes géométriques.
-    Cette classe fournit des méthodes pour valider les entiers et
-    calculer la surface des formes géométriques.
+    Base class for geometric shapes.
+    This class provides methods for validating integers and computing
+    the area of geometric shapes.
     """
 
     def area(self):
         """
-        Méthode pour calculer la surface de la forme géométrique.
-        Cette méthode doit être implémentée par les sous-classes.
-        Raises:
-            Exception: Si la méthode n'est pas implémentée dans une sous-classe.
-        """
+        Computes the area of the geometric shape.
+        This method is intended to be implemented by subclasses.
 
+        Raises:
+            Exception: If the method is not implemented by a subclass.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
-        Valide que `value` est un entier positif.
-        Args:
-            name (str): Le nom de la variable à valider.
-            value (int): La valeur à valider.
-        Raises:
-            TypeError: Si `value` n'est pas un entier.
-            ValueError: Si `value` n'est pas supérieur à 0.
-        """
+        Validates that `value` is a positive integer.
 
+        Args:
+            name (str): The name of the variable to validate.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is not greater than 0.
+        """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
 
