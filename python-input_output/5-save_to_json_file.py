17af38a0f87468a564c0c3169f8c7f5c41139373
IEyZens <roncint61@gmail.com>
2025-06-04 16:05:41 +0200

    Improve docstring clarity in save_to_json_file function

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -17,5 +17,8 @@ def save_to_json_file(my_obj, filename):
         filename (str): The name of the file to write the JSON representation
         into.
     """
+    # Open the file with the specified filename in write mode
+    # and ensure it is encoded in UTF-8.
     with open(filename, "w", encoding="utf-8") as file:
+        # Use json.dump to write the Python object to the file in JSON format.
         json.dump(my_obj, file)
