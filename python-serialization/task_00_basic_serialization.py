49392d9a7ca414707a84c3ea5dabfaff8bb6a408
IEyZens <roncint61@gmail.com>
2025-06-05 14:27:14 +0200

    :bulb: Enhance comments for clarity in serialization and deserialization functions

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -16,7 +16,9 @@ def serialize_and_save_to_file(data, filename):
 
     This function overwrites the file if it already exists.
     """
+    # Ensure the data is a dictionary
     with open(filename, "w", encoding="utf-8") as file:
+        # Serialize the dictionary to JSON and write it to the file
         json.dump(data, file)
 
 
@@ -30,6 +32,9 @@ def load_and_deserialize(filename):
     Returns:
         dict: The Python dictionary resulting from the JSON content.
     """
+    # Open the file and read its content
     with open(filename, "r", encoding="utf-8") as file:
+        # Deserialize the JSON content into a Python dictionary
         new_dic = json.load(file)
+        # Return the deserialized dictionary
         return new_dic
