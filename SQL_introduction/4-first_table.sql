a6b4ec9e065a3d7895453a5d3f48fe6dc7206c6a
IEyZens <roncint61@gmail.com>
2025-06-24 01:35:53 +0200

    :hammer: Fix SQL syntax and enhance comments in 4-first_table.sql for clarity

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,9 @@
+-- Create a table named 'first_table' only if it doesn't already exist
+-- The table contains:
+--   - 'id': an integer column
+--   - 'name': a variable-length string column with a maximum of 256 characters
 CREATE TABLE IF NOT EXISTS first_table
-{
+(
 	id INT,
 	name VARCHAR(256)
-}
+)
