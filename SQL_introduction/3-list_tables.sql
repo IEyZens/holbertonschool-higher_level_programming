af440b611949e3c04e9b292be3bb7ea8cfb9e961
IEyZens <roncint61@gmail.com>
2025-06-23 16:05:08 +0200

    :hammer: Update 3-list_tables.sql to create database 'hbtn_0c_0' and show its tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,5 @@
--- Display all the tables contained in the 'mysql' database
-SHOW TABLES FROM mysql
+-- Create the database 'hbtn_0c_0' only if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+
+-- Show all the tables currently present in the 'hbtn_0c_0' database
+SHOW TABLES FROM hbtn_0c_0
