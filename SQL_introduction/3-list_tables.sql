6bef9e74a3450a8fe84009dde0baf4ce3ecee0b1
IEyZens <roncint61@gmail.com>
2025-06-24 01:31:31 +0200

    Remove database creation and use statements from 3-list_tables.sql

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,8 +1,2 @@
--- Create the database 'hbtn_0c_0' only if it does not already exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- Switch to the 'hbtn_0c_0' database to make it the default for subsequent operations
-USE hbtn_0c_0;
-
 -- List all the tables currently present in the 'hbtn_0c_0' database
 SHOW TABLES;
