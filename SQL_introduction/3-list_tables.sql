7131be971e4677955b2c5a0113449da54ab1ffbd
IEyZens <roncint61@gmail.com>
2025-06-23 16:09:41 +0200

    :hammer: Fix SQL script to properly terminate statements and clarify comments for database creation and table listing

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,8 @@
 -- Create the database 'hbtn_0c_0' only if it does not already exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
--- Show all the tables currently present in the 'hbtn_0c_0' database
-SHOW TABLES FROM hbtn_0c_0
+-- Switch to the 'hbtn_0c_0' database to make it the default for subsequent operations
+USE hbtn_0c_0;
+
+-- List all the tables currently present in the 'hbtn_0c_0' database
+SHOW TABLES;
