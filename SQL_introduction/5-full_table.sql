d093cd42852ed67b4eba9705f972ca6f3ed11025
IEyZens <roncint61@gmail.com>
2025-06-24 01:50:48 +0200

    :hammer: Specify schema in SQL query to retrieve column metadata for 'first_table'

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Retrieve metadata about all columns in the table named 'first_table'
 -- This includes column names, data types, defaults, etc.
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'first_table'
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'first_table' AND TABLE_SCHEMA = 'hbtn_0c_0'
