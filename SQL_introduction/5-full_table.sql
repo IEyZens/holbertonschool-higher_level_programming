f816ac69f154faefc73363eea2694de1bb5bf4ef
IEyZens <roncint61@gmail.com>
2025-06-24 01:49:54 +0200

    :hammer: Enhance comments in 5-full_table.sql for clarity on column metadata retrieval

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,1 +1,3 @@
+-- Retrieve metadata about all columns in the table named 'first_table'
+-- This includes column names, data types, defaults, etc.
 SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'first_table'
