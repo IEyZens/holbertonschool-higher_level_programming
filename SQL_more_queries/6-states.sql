23c77555ab61a5c36786acd1d6f280b2fece552b
IEyZens <roncint61@gmail.com>
2025-06-25 01:57:07 +0200

    🔨 Remove redundant comments in '6-states.sql' for clarity

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,10 +1,7 @@
--- Create the database 'hbtn_0d_usa' if it does not already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Create the table 'states' only if it doesn't already exist, inside the currently selected database
--- The table includes:
---   - 'id': an auto-incrementing primary key that cannot be NULL
---   - 'name': a string column (up to 256 characters) that must not be NULL
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states
 (
 	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
