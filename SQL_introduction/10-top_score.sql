5147d0fafa98292b6faed3049dff690b5cd4081b
IEyZens <roncint61@gmail.com>
2025-06-24 02:39:33 +0200

    :hammer: Change sorting order in '10-top_score.sql' to descending for top scores

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Retrieve the 'score' and 'name' columns from 'second_table'
 -- Sort the results by 'score' in descending order (highest score first)
-SELECT SCORE, NAME FROM second_table ORDER BY SCORE ASC LIMIT 10;
+SELECT SCORE, NAME FROM second_table ORDER BY SCORE DESC LIMIT 10;
