91f4e1f14fb32176ac2122003b621eed91cb5ef9
IEyZens <roncint61@gmail.com>
2025-06-24 02:29:24 +0200

    ✨ Create 'second_table' with initial data insertion

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,18 @@
+-- Create the table 'second_table' only if it doesn't already exist
+-- It has three columns:
+--   - 'id': an integer
+--   - 'name': a string up to 256 characters
+--   - 'score': an integer
+CREATE TABLE IF NOT EXISTS second_table
+(
+	id INT,
+	name VARCHAR(256),
+	score INT
+)
+
+-- Insert four rows into 'second_table' with specified id, name, and score values
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
