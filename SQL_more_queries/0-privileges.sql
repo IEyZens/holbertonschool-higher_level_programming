5af393ebf469e7c8a0cfba5cd030669d9be4033e
IEyZens <roncint61@gmail.com>
2025-06-25 01:10:03 +0200

    ✨ Add SQL script to show privileges for users 'user_0d_1' and 'user_0d_2'

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Show the privileges granted to the user 'user_0d_1' connecting from 'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show the privileges granted to the user 'user_0d_2' connecting from 'localhost'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
