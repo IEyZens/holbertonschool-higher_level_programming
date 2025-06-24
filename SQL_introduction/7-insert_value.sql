c868666409be2b3372abc91325f454d47720b166
IEyZens <roncint61@gmail.com>
2025-06-24 02:18:49 +0200

    :hammer: Add SQL script to insert a new row into 'first_table'

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Insert a new row into 'first_table' with:
+--   - id = 89
+--   - name = 'Best School'
+-- The other columns ('c' and 'created_at') will be set to NULL unless defaults are defined
+INSERT INTO first_table (id, name) VALUES (89, 'Best School')
