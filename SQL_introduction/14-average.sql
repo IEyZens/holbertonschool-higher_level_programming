5a6f47b289dc55c311d77ccec2ebac908479b49b
IEyZens <roncint61@gmail.com>
2025-06-24 02:54:23 +0200

    🔨 Update SQL query in '14-average.sql' to limit results to 10 entries

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Calculate the average value of the 'score' column from 'second_table'
 -- Returns a single value representing the mean of all non-null scores
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) FROM second_table LIMIT 10;
