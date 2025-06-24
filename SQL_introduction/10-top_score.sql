c9fbf34a4e75ceedbd00debab5c0d590c8ef8019
IEyZens <roncint61@gmail.com>
2025-06-24 02:38:00 +0200

    ✨ Add SQL query to retrieve and sort scores from 'second_table'

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Retrieve the 'score' and 'name' columns from 'second_table'
+-- Sort the results by 'score' in descending order (highest score first)
+SELECT SCORE, NAME FROM second_table ORDER BY SCORE DESC;
