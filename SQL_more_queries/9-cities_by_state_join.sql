88e20a298747d92b1903792158e60c177fdcf8e5
IEyZens <roncint61@gmail.com>
2025-06-25 15:05:50 +0200

    🔨 Format SQL query for better readability

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -3,4 +3,7 @@
 --   - the 'name' from the 'states' table
 -- Only include rows where the city's state_id matches the state's id (join condition)
 -- Sort the results by 'cities.id' in ascending order
-SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
