8c782e70e45666f74706a52d8b18824b9ff2abe6
IEyZens <roncint61@gmail.com>
2025-06-25 15:02:32 +0200

    ✨ Add SQL script to retrieve city and state names with join condition

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Retrieve:
+--   - the 'id' and 'name' from the 'cities' table
+--   - the 'name' from the 'states' table
+-- Only include rows where the city's state_id matches the state's id (join condition)
+-- Sort the results by 'cities.id' in ascending order
+SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC
