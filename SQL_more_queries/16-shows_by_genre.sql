0854fed33779b5bf1d070e0f3cf24909b508b046
IEyZens <roncint61@gmail.com>
2025-06-26 10:37:27 +0200

    🔨 Refactor comments for clarity in TV shows genre retrieval SQL script

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,8 @@
+-- Select the title of each TV show and its associated genre_id
+-- Use a LEFT JOIN to ensure all TV shows are included,
+-- even those that do not have any associated genres (genre_id will be NULL in that case)
+-- Sort the results first alphabetically by TV show title,
+-- then by genre_id in ascending order (NULLs appear last by default)
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
