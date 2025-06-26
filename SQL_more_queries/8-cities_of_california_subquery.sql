52d276db36263ed1cd940415408aa929b1b56715
IEyZens <roncint61@gmail.com>
2025-06-26 13:46:52 +0200

    🔨 Refactor SQL query to select specific columns and simplify ordering

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- Select the id, name, and state_id of cities that belong to the state named 'California'
 -- Filter to include only cities where the state_id matches the id of the state 'California'
 -- Sort the results by city id in ascending order
-SELECT cities.id, cities.name, cities.state_id
+SELECT id, name
 FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY cities.id ASC;
+ORDER BY id ASC;
