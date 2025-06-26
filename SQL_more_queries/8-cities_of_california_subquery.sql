3d199c06222de4909883335d585cb5cd0e9f6ab5
IEyZens <roncint61@gmail.com>
2025-06-26 09:36:36 +0200

    🔨 Format SQL query for better readability

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,7 @@
 -- Select all columns from the 'cities' table
 -- Only include rows where the 'state_id' matches the 'id' of the state named 'California'
 -- Results are sorted by 'id' in ascending order
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC
+SELECT *
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC
