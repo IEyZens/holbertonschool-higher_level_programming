07c089cda7d04618f3272efba77166116ff5ff2c
IEyZens <roncint61@gmail.com>
2025-06-26 15:17:53 +0200

    🔨 Simplify SQL query by removing redundant table reference in ORDER BY clause

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -23,9 +23,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     # Execute the SQL query to select states starting with 'N'
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-    )
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     # Fetch and print all matching rows
     for row in cur.fetchall():
