76438d5841937a57912ef5815450096c235d3e93
IEyZens <roncint61@gmail.com>
2025-06-28 18:49:36 +0200

    🔨 Add ORDER BY clause to SQL query for consistent result sorting

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -23,7 +23,8 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE BINARY name=%s", (sys.argv[4],))
+        "SELECT * FROM states WHERE BINARY name=%s ORDER BY states.id ASC",
+        (sys.argv[4],))
 
     for row in cur.fetchall():
         print(row)
