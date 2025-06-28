87255870fd6de274fe983c0d30892be92148624b
IEyZens <roncint61@gmail.com>
2025-06-28 11:08:06 +0200

    🔨 Modify SQL query to enforce case sensitivity for state names starting with 'N'

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC"
     )
 
     for row in cur.fetchall():
