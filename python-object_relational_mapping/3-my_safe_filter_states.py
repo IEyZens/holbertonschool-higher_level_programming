b47d8ca484c80019b73aac439b6085cb4435b378
IEyZens <roncint61@gmail.com>
2025-06-28 18:45:06 +0200

    🔨 Fix SQL query syntax for state name filtering

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE BINARY name=%s ORDER BY states.id ASC",
+        "SELECT * FROM states WHERE BINARY name='%s' ORDER BY states.id ASC",
         (sys.argv[4],)
     )
 
