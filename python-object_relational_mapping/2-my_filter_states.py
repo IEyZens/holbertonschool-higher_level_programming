2de60e7700d9c30136920d9f0127ed77c4e8af95
IEyZens <roncint61@gmail.com>
2025-06-28 18:27:13 +0200

    ✨ Add script to display values from states table based on user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script that displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+
+Usage:
+    ./2-my_filter_states.py <mysql_username> <mysql_password> <database_name> <state_name>
+
+Connects to a MySQL server running on localhost at port 3306.
+Uses format to create the SQL query with the user input.
+Results are sorted in ascending order by states.id.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1],
+        password=sys.argv[2], db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC"
+        .format(sys.argv[4])
+    )
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
