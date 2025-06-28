4dba77a3868b48768ab82a19526c61791be613a0
IEyZens <roncint61@gmail.com>
2025-06-28 18:43:32 +0200

    ✨ Add script to filter states by name with case sensitivity

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1],
+        password=sys.argv[2], db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE BINARY name=%s ORDER BY states.id ASC",
+        (sys.argv[4],)
+    )
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
