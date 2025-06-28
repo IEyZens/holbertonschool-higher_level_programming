8194015f9d526208be2272e51ad760f1fd86f842
IEyZens <roncint61@gmail.com>
2025-06-28 18:34:42 +0200

    🔨 Modify SQL query to enforce case sensitivity for state names

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC"
+        "SELECT * FROM states WHERE BINARY name='{}' ORDER BY states.id ASC"
         .format(sys.argv[4])
     )
 
