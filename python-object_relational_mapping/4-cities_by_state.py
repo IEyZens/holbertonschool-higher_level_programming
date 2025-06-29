4edb1601edc029fe4139f333b68b47932aa3b24e
IEyZens <roncint61@gmail.com>
2025-06-29 10:10:37 +0200

    Add WHERE clause to SQL query to filter cities by state name

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
     cur.execute(
         "SELECT cities.id, cities.name, states.name FROM cities "
         "INNER JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
         "ORDER BY cities.id ASC"
     )
 
