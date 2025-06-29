845cec2995e7e19b592099dad01ae5dfd3c752ec
IEyZens <roncint61@gmail.com>
2025-06-29 14:04:36 +0200

    Update docstring in 8-model_state_fetch_first.py for clarity and usage instructions

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,3 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that prints the first State object from a MySQL database using SQLAlchemy.
+
+Usage:
+    ./8-model_state_fetch_first.py <username> <password> <database>
+
+Arguments:
+    username    : MySQL username
+    password    : MySQL password
+    database    : Name of the target database
+
+Functionality:
+    - Connects to the database using SQLAlchemy
+    - Retrieves and displays the first State object by id
+    - If no state exists, prints 'Nothing'
+
+Example:
+    ./8-model_state_fetch_first.py root root123 hbtn_0e_6_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
