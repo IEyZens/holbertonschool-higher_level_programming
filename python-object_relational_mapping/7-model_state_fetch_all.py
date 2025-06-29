c17f131d2c6630fd9e55d55468e454128580e311
IEyZens <roncint61@gmail.com>
2025-06-29 13:45:40 +0200

    Refactor state fetching logic to improve readability and streamline session management

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -20,21 +20,23 @@ Example:
 """
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 import sys
 
 if __name__ == "__main__":
 
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}"
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True,
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
     )
 
-    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    session = Session(engine)
+    states = session.query(State).order_by(State.id).all()
 
-    for state in session.query(State).order_by(State.id).all():
+    for state in states:
         print("{}: {}".format(state.id, state.name))
 
     session.close()
