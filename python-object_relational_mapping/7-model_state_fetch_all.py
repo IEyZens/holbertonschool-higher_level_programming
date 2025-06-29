f1cb2c108681439c3f3074c65b29fd2adb9c9788
IEyZens <roncint61@gmail.com>
2025-06-29 13:35:44 +0200

    Refactor database connection setup to use Session directly and simplify engine creation

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -20,20 +20,19 @@ Example:
 """
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 import sys
 
 if __name__ == "__main__":
 
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        "mysql+mysqldb://{}:{}@localhost/{}"
         .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
     )
 
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session(engine)
 
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
