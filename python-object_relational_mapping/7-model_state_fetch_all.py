39642a52adca6ce50e1830bff21379a2464e236b
IEyZens <roncint61@gmail.com>
2025-06-29 13:34:01 +0200

    Refactor code to streamline state retrieval and printing

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -30,12 +30,12 @@ if __name__ == "__main__":
         .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
     )
 
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).order_by(State.id).all()
-
-    for s in state:
-        print("{}: {}".format(s.id, s.name))
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
