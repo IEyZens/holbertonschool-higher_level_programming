95fcfb8640a39a8c6ae947c9a009f2cb151fd8b9
IEyZens <roncint61@gmail.com>
2025-06-29 10:30:22 +0200

    Fix SQL query formatting by adding a space before the ORDER BY clause for consistency

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -22,7 +22,8 @@ if __name__ == "__main__":
 
     cur.execute(
         "SELECT cities.id, cities.name, states.name "
-        "FROM cities INNER JOIN states ON cities.state_id = states.id"
+        "FROM cities INNER JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
     )
 
     for row in cur.fetchall():
