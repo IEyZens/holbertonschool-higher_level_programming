f5b7e108f15bb010f87866dd0cc363e883b42f81
IEyZens <roncint61@gmail.com>
2025-06-29 10:29:32 +0200

    Remove unnecessary ORDER BY clause from SQL query for simplicity

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -22,8 +22,7 @@ if __name__ == "__main__":
 
     cur.execute(
         "SELECT cities.id, cities.name, states.name "
-        "FROM cities INNER JOIN states ON cities.state_id = states.id "
-        "ORDER BY cities.id ASC"
+        "FROM cities INNER JOIN states ON cities.state_id = states.id"
     )
 
     for row in cur.fetchall():
