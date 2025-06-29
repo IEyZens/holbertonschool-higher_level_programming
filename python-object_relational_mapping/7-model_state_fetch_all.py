3734879d55614d0016f75471cb4c3f5c9acee0d1
IEyZens <roncint61@gmail.com>
2025-06-29 13:28:16 +0200

    Remove argument check for script usage to streamline execution

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -25,10 +25,6 @@ import sys
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 4:
-        print("Usage: ./script.py <username> <password> <database>")
-        sys.exit(1)
-
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}"
         .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
