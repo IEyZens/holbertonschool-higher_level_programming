65e80337176b2a13e11ca527f0df89c102f7a170
IEyZens <roncint61@gmail.com>
2025-06-29 10:27:40 +0200

    Refactor SQL query to use INNER JOIN for clarity

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     cur.execute(
         "SELECT cities.id, cities.name, states.name "
-        "FROM cities JOIN states ON cities.state_id = states.id "
+        "FROM cities INNER JOIN states ON states.id = cities.state_id "
         "ORDER BY cities.id ASC"
     )
 
