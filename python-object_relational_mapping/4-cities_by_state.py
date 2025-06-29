b0dad0d349dea9b48455888679e076b7889b5b08
IEyZens <roncint61@gmail.com>
2025-06-29 10:05:55 +0200

    Fix SQL query formatting by removing unnecessary spaces for improved readability

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
 
     # Execute a JOIN query to get city and corresponding state names
     cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities "
-        "INNER JOIN states ON cities.state_id = states.id "
+        "SELECT cities.id, cities.name, states.name FROM cities"
+        "INNER JOIN states ON cities.state_id = states.id"
         "ORDER BY cities.id ASC"
     )
 
