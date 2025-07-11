eaa346578f0a4f4dab204ea96a46ea7b05edba3e
IEyZens <roncint61@gmail.com>
2025-07-11 14:18:01 +0200

    Refactor argument handling to improve output messages based on argument count

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,6 +2,10 @@
 
 const { argv } = require('node:process');
 
-argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
-});
+if (argv === 0) {
+  console.log('No argument');
+} else if (argv === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
