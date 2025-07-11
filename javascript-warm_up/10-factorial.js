c6a8ca809cbe9ae8724a198dd7f9f92dd0ccb45a
IEyZens <roncint61@gmail.com>
2025-07-11 15:15:38 +0200

    Add 10-factorial.js script to compute the factorial of a number from command line input

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+function factorial(n) {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const number = parseInt(argv[2]);
+console.log(factorial(number));
