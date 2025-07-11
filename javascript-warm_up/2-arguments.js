6d2fd08b3a2c51e0144b7f9e833be4937469adb1
IEyZens <roncint61@gmail.com>
2025-07-11 14:22:42 +0200

    Fix argument checks to correctly identify the number of command line arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const { argv } = require('node:process');
 
-if (argv === 0) {
+if (argv.length === 2) {
   console.log('No argument');
-} else if (argv === 1) {
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
