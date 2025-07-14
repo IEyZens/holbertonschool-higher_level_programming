ebc6dc4976cf06db0c5926959a193bbe0fa3feb7
IEyZens <roncint61@gmail.com>
2025-07-14 04:24:48 +0200

    Add script to change header color to red on click

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,9 @@
+document.querySelector("html")
+const div = document.getElementById("red_header").onclick = function() {clickFunction()};
+
+function clickFunction() {
+  const header = document.getElementsByTagName("header");
+  for (let i = 0; i < header.length; i++) {
+    header[i].style.color = "#FF0000";
+  }
+}
